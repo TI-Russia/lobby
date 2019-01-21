c1026e51231d65971c964a40c7dc27c5d1ea66be
MainOlma <mainolma@gmail.com>
2019-01-21 13:01:04 +0400

    make toggle button to show nav bar on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,6 +30,18 @@ require(["d3","jquery","intro","chart","vk"], function(d3,$,intro,chart,vk) {
     vk
     document.getElementById('vk_share_button').innerHTML = VK.Share.button({noparse:false}, {type: 'custom ', text:"<span class='icon is-large'><i class='fab fa-vk'></i></span>"});
 
+    document.getElementById("nav-toggle").addEventListener ("click", toggleNav);
+    function toggleNav() {
+        var nav = document.getElementById("nav-menu");
+        var className = nav.getAttribute("class");
+        if(className == "is-visible-small is-hidden") {
+            nav.className = "is-visible-small";
+            document.getElementById("header-title").className="column c1 is-hidden"
+        } else {
+            document.getElementById("header-title").className="column c1"
+            nav.className = "is-visible-small is-hidden" ;
+        }
+    }
 
 });
 
