35471a5acd4f0fcf97e29362ff3ea185e7995018
MainOlma <mainolma@gmail.com>
2019-01-21 17:29:13 +0400

    set cookie for 2 hours delay of intro showing

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,8 @@ require.config({
         ShowCard: "modules/card",
         data: "modules/data",
         ShowedClusters: "tools/showed_clusters",
-        zoom:"tools/zoom"
+        zoom:"tools/zoom",
+        cookie:"tools/cookie"
     },
     shim: {
         awesomeplete:{
@@ -22,11 +23,26 @@ require.config({
     }
 });
 
-require(["d3","jquery","intro","chart"], function(d3,$,intro,chart) {
+require(["d3","jquery","intro","chart","cookie"], function(d3,$,intro,chart,cookie) {
     window.d3 = d3;
     intro
     chart
+    /*Set Cookie for Intro*/
+    var dateOfOpen = new Date(new Date().getTime());
+    var date = new Date(new Date().getTime() + 3600*2 * 1000);//date+interval 2 рщгкы
+    var cook = cookie.getCookie("dateOfOpen")
+    if (cook==undefined) {
+        cookie.setCookie("c", "set_cookie_for_2_hours");
+        cookie.setCookie("dateOfOpen", dateOfOpen.toUTCString(), {expires: date.toUTCString()})
+        $('.cd-intro').show();
+    }
+    else {
+        cookie.setCookie("c", "cookie_already_was_here");
+        $('.cd-intro').hide();
+        $('body').removeClass('intro-open')
+    }
 
+    /*Mobile burger-button menu toggler*/
     document.getElementById("nav-toggle").addEventListener ("click", toggleNav);
     function toggleNav() {
         var nav = document.getElementById("nav-menu");
