d093cc04b9ce84fc2e7f8699d25e46db9c47d7ea
MainOlma <mainolma@gmail.com>
2019-01-22 15:29:27 +0400

    fix cookie set event

--- assets/js/modules/intro.js
+++ assets/js/modules/intro.js
@@ -1,14 +1,24 @@
 requirejs(['jquery','cookie'], function( $,cookie ) {
     $('#clusters').addClass('is-loading');
+
+
     /*Set Cookie for Intro*/
     var dateOfOpen = new Date(new Date().getTime());
     var date = new Date(new Date().getTime() + 3600*2 * 1000);//date+interval 2 рщгкы
     var cook = cookie.getCookie("dateOfOpen")
     if (cook==undefined) {
-        //set_cookie_for_2_hours
-        cookie.setCookie("dateOfOpen", dateOfOpen.toUTCString(), {expires: date.toUTCString()})
         $('body').addClass('intro-open')
         $('.cd-intro').show();
+        $('.main-action').click(
+            function (e) {
+                e.preventDefault();
+                //set_cookie_for_2_hours
+                cookie.setCookie("dateOfOpen", dateOfOpen.toUTCString(), {expires: date.toUTCString()})
+                $('.cd-intro').hide();
+                $('body').removeClass('intro-open')
+            })
+
+
     }
     else {
         //cookie_already_was_here
