ed3e2353d5c8a4e7f1fcd21eb8bce3f72b4dee3f
Pavel Garaev <ipagar@icloud.com>
2024-04-02 22:09:57 +0400

    Refactor burger.js to use ES6 syntax and improve code readability

--- _src/tools/burger.js
+++ _src/tools/burger.js
@@ -1,20 +1,22 @@
-import $ from 'jquery';
-    
+import $ from "jquery";
+
 /*Mobile burger-button menu toggler*/
-$(document).ready(function() {
-    document.getElementById("nav-toggle").addEventListener("click", toggleNav);
+$(document).ready(function () {
+  const navToggle = document.getElementById("nav-toggle");
+  if (navToggle) {
+    navToggle.addEventListener("click", toggleNav);
+  }
 
-    function toggleNav() {
-        const nav_toggle = document.getElementById("nav-toggle");
-        const menu = document.getElementById("header_menu");
+  function toggleNav() {
+    const nav_toggle = document.getElementById("nav-toggle");
+    const menu = document.getElementById("header_menu");
 
-        if (nav_toggle.className == "") {
-            nav_toggle.className = "is-active is-visible";
-            menu.className = "header-menu is-visible";
-        } else {
-            nav_toggle.className = "";
-            menu.className = "header-menu";
-        }
+    if (nav_toggle.className == "") {
+      nav_toggle.className = "is-active is-visible";
+      menu.className = "header-menu is-visible";
+    } else {
+      nav_toggle.className = "";
+      menu.className = "header-menu";
     }
+  }
 });
-
