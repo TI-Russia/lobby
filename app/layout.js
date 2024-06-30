3e0c5583bae9ff9285058fff1e0fad5b6e5ce2c7
Pavel Garaev <ipagar@icloud.com>
2024-06-30 15:14:03 +0400

    chore: Add Font Awesome script for icons

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,12 @@ config.autoAddCss = false;
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://use.fontawesome.com/releases/v5.15.1/js/all.js"
+        ></script>
+      </head>
       <body>
         <Header pageType={"duma_8"} />
         {children}
