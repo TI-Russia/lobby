9bfbdb4c10ba06a36166ebd601f5ab4d4f3604b5
Pavel Garaev <ipagar@icloud.com>
2024-08-08 20:02:58 +0400

    Add metrics

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import Footer from "./footer";
 import { Header } from "./header";
 import { config } from "@fortawesome/fontawesome-svg-core";
@@ -8,6 +9,56 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-RB4VVKFKG4"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag("js", new Date());
+          gtag("config", "G-RB4VVKFKG4");
+        `}
+        </Script>
+        <script>
+          {`
+            <!-- Yandex.Metrika counter -->
+            <script type="text/javascript" >
+                (function (d, w, c) {
+                    (w[c] = w[c] || []).push(function() {
+                        try {
+                            w.yaCounter51964349 = new Ya.Metrika({
+                                id:51964349,
+                                clickmap:true,
+                                trackLinks:true,
+                                accurateTrackBounce:true
+                            });
+                        } catch(e) { }
+                    });
+
+                    var n = d.getElementsByTagName("script")[0],
+                        x = "https://mc.yandex.ru/metrika/watch.js",
+                        s = d.createElement("script"),
+                        f = function () { n.parentNode.insertBefore(s, n); };
+                    for (var i = 0; i < document.scripts.length; i++) {
+                        if (document.scripts[i].src === x) { return; }
+                    }
+                    s.type = "text/javascript";
+                    s.async = true;
+                    s.src = x;
+
+                    if (w.opera == "[object Opera]") {
+                        d.addEventListener("DOMContentLoaded", f, false);
+                    } else { f(); }
+                })(document, window, "yandex_metrika_callbacks");
+            </script>
+            <noscript><div><img src="https://mc.yandex.ru/watch/51964349" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+            <!-- /Yandex.Metrika counter -->
+            `}
+        </script>
         <script
           async
           src="https://use.fontawesome.com/releases/v5.15.1/js/all.js"
